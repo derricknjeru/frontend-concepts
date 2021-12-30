04721087f33ddffbec132254e4208ca2e2295aab
derricknjeru <derricknjeru@microsoft.com>
2021-12-30 10:03:39 +0300

    adding types

--- typescript-getting-started/app/app.ts
+++ typescript-getting-started/app/app.ts
@@ -1,7 +1,21 @@
 function startGame() {
   //comments
-  var messageElement = document.getElementById("messages");
+  let playerName: string = "Derrick Njeru";
+
+  //type casting and assertion
+  let no: any = 5;
+  let x: string = (no as number).toFixed(4);
+  let y: string = (<number>no).toFixed(4);
+
+
+  logPlayer(playerName);
+
+  let messageElement = document.getElementById("messages");
   messageElement!.innerHTML = "Welcome to MultiMath, starting a new game";
+}
+
+function logPlayer(name: string | undefined) {
+  console.log(`New game starting for player:${name}`);
 }
 
 document.getElementById("startGame")!.addEventListener("click", startGame);
