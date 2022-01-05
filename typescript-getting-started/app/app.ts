465b1013e54a772df7791b5a0dc342e934953201
derricknjeru <derricknjeru5@gmail.com>
2022-01-05 21:46:43 +0300

    creating and using custom types

--- typescript-getting-started/app/app.ts
+++ typescript-getting-started/app/app.ts
@@ -1,52 +1,21 @@
-function startGame() {
-  //comments
-  let playerName: string | undefined = getInputValue("playername");
+/// <reference path="player.ts" />
+/// <reference path="game.ts" />
 
-  //type casting and assertion
-  let no: any = 5;
-  let x: string = (no as number).toFixed(4);
-  let y: string = (<number>no).toFixed(4);
+let newGame: Game;
 
-  logPlayer(playerName);
+// add click handler to the start game button
+document.getElementById('startGame')!.addEventListener('click', () => {
+  const player: Player = new Player();
+  player.name = Utility.getInputValue('playername');
 
-  postScore(100, playerName);
-  postScore(-1, playerName);
-}
+  const problemCount: number = Number(Utility.getInputValue('problemCount'));
+  const factor: number = Number(Utility.getInputValue('factor'));
 
-function logPlayer(name: string = "MultiMath player"): void {
-  console.log(`New game starting for player:${name}`);
-}
+  newGame = new Game(player, problemCount, factor);
+  newGame.displayGame();
+});
 
-function getInputValue(elementID: string): string | undefined {
-  const inputElement: HTMLInputElement = <HTMLInputElement>(
-    document.getElementById(elementID)
-  );
-  if (inputElement.value == "") {
-    return undefined;
-  } else {
-    return inputElement.value;
-  }
-}
-
-function postScore(
-  score: number,
-  playerName: string = "MultiMath player"
-): void {
-  let logger: (value: string) => void; //can be assigned any function that takes a string and return null
-
-  if (score < 0) logger = logError;
-  else logger = logMessage;
-
-  const scoreElement: HTMLElement | null =
-    document.getElementById("postedScores");
-  scoreElement!.innerText = `${score} - ${playerName}`;
-
-  logger(`Score ${score}`);
-}
-document.getElementById("startGame")!.addEventListener("click", startGame);
-
-const logMessage = (message: string) => console.log(message);
-
-function logError(err: string): void {
-  console.error(err);
-}
+// add click handler to the calculate score button
+document.getElementById('calculate')!.addEventListener('click', () => {
+  newGame.calculateScore();
+});
